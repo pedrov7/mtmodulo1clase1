ce970ee47a92e29a86cb74152b032f134cd6da46
pedrov7 <pedro7valla@HOTMAIL.COM>
2023-03-21 22:09:33 -0500

    use StringBuilder

--- src/main/java/killer/com/firstclassmt01/app/Program.java
+++ src/main/java/killer/com/firstclassmt01/app/Program.java
@@ -1,5 +1,6 @@
 package killer.com.firstclassmt01.app;
 
+import java.awt.event.ItemEvent;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -7,7 +8,9 @@ import java.util.Scanner;
 
 import killer.com.firstclassmt01.model.Client;
 import killer.com.firstclassmt01.model.Order;
+import killer.com.firstclassmt01.model.OrderItem;
 import killer.com.firstclassmt01.model.OrderStatus;
+import killer.com.firstclassmt01.model.Product;
 
 public class Program {
 
@@ -22,17 +25,40 @@ public class Program {
 		System.out.println("Birth date: (YYYY-MM-DD)");
 		String birthDateString = scanner.nextLine();
 		LocalDate birthDate = LocalDate.parse(birthDateString);
-		
-		Client client= new Client(name, email, birthDate);
-		
+
+		Client client = new Client(name, email, birthDate);
+
 		System.out.println("Enter order data");
 		System.out.println("Status");
-		
+
 		OrderStatus status = OrderStatus.valueOf(scanner.nextLine());
+
+		Order order = new Order(LocalDateTime.now(), status, client, new ArrayList<>()); // LocalDateTime saca la fecha
+																							// del sistema
+
+		System.out.println("How many items to this order?");
+
+		int numberOfItems = scanner.nextInt();
+
+		for (int i = 1; i <= numberOfItems; i++) {
+			System.out.println("Enter # " +i+ " item data =>");
+			System.out.println("Product name: ");
+			scanner.nextLine(); 		//artificio para que leer el teclado sin que se salte la primera lectura
+			String productName = scanner.nextLine();
+			System.out.println("Product price: ");
+			double productPrice = scanner.nextDouble();
+			System.out.println("Quantity: ");
+			int quantity = scanner.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			OrderItem item = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(item);
+		}
 		
-		Order order = new Order(LocalDateTime.now(), status, client, new ArrayList<>());
-		
-		
+		System.out.println("ORDER SUMMARY");
+		System.out.println(order);
+		scanner.close();
 
 	}
 
--- src/main/java/killer/com/firstclassmt01/model/Order.java
+++ src/main/java/killer/com/firstclassmt01/model/Order.java
@@ -15,10 +15,51 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class Order {
-	
-	private LocalDateTime moment;   //dato de tpo fecha con la hora y fecha de creacion
-	private OrderStatus status;		//el tipo OrderStatus va a ser un ENUM
+
+	private LocalDateTime moment; // dato de tipo fecha con la hora y fecha de creacion
+	private OrderStatus status; // el tipo OrderStatus va a ser un ENUM
 	private Client client;
 	private List<OrderItem> items;
+
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+
+	public double total() {
+		double sum = 0;
+		// for enhacement
+		for (OrderItem it : items) {
+			sum += it.getPrice() * it.getQuantity();
+		}
+		return sum;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append("Order moment: ");
+		sBuilder.append(moment).append("\n");
+		sBuilder.append("Order status: ");
+		sBuilder.append(status).append("\n");
+		sBuilder.append(client).append("\n");
+		sBuilder.append("Order items: \n");
+
+		for (OrderItem it : items) {
+			sBuilder.append(it).append("\n");
+		}
+
+		sBuilder.append("Total price: $");
+		sBuilder.append(String.format("%.2f", total()));
+		return sBuilder.toString();
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Order ["
+//				+ "moment=" + moment + 
+//				", status=" + status + 
+//				", client=" + client + 
+//				", items=" + items + "]";
+//	}
 
 }
